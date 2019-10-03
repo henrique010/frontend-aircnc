8f205bc069bd09c1099677b43cc71ddb3da5b1e6
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2019-10-03 13:10:18 -0300

    login page and api connection created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import api from './services/api';
+
+import logo from './assets/logo.svg';
+
 export default function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail *</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            placeholder="Seu melhor e-mail" />
+
+          <button type="submit" className="submit-button">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
